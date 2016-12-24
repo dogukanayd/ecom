6264e6c531931e547d14654b5dfc5343227946ad
dogukanayd <daydogdu93@gmail.com>
2016-12-24 20:12:17 +0300

    SendingMessages

--- resources/functions.php
+++ resources/functions.php
@@ -170,7 +170,22 @@ function login_user(){
 
 function send_message(){
     if(isset($_POST['submit'])){
-        echo "It works";
+        $to = "daydogdu93@gmail.com";
+        $from_name =  $_POST['name'];
+        $subject =  $_POST['subject'];
+        $email =  $_POST['email'];
+        $message =  $_POST['message'];
+        $headers = "From: {$from_name} {$email}";
+
+        $result = mail($to,$subject,$message,$headers);
+        if(!$result){
+            set_message("Sorry we could not send your message");
+            redirect("contact.php");
+        }else{
+            set_message("Your Message has been sent");
+            redirect("contact.php");
+        }
+
     }
 }
 
